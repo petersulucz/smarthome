ab2586ea32138d71d068e9605097deac67455fa6
Peter Sulucz <peter.sulucz@gmail.com>
2016-05-31 21:14:20 -0700

    Added ability to load homes

--- Source/Backend/HomeHub.Data.Common/IDataLayer.cs
+++ Source/Backend/HomeHub.Data.Common/IDataLayer.cs
@@ -1,14 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HomeHub.Common.Devices;
-
-namespace HomeHub.Data.Common
+﻿namespace HomeHub.Data.Common
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using HomeHub.Common.Devices;
+    using HomeHub.Data.Common.Models;
+
+    /// <summary>
+    /// The DataLayer interface.
+    /// </summary>
     public interface IDataLayer
     {
+        /// <summary>
+        /// Get all devices for a user.
+        /// </summary>
+        /// <param name="user">The user.</param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
         Task<IEnumerable<Device>> GetAllDevices(Guid user);
+
+        /// <summary>
+        /// The create home.
+        /// </summary>
+        /// <param name="home">The home.</param>
+        /// <param name="user">The user.</param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        Task<Home> CreateHome(Home home, Guid user);
+
+        /// <summary>
+        /// Gets the list of all homes accessible to a user
+        /// </summary>
+        /// <param name="user">The user id</param>
+        /// <returns>The list of accessible homes</returns>
+        Task<IEnumerable<Home>> GetHomes(Guid user);
     }
 }
--- Source/Backend/HomeHub.Data.Sql/SqlDataLayer.cs
+++ Source/Backend/HomeHub.Data.Sql/SqlDataLayer.cs
@@ -1,36 +1,174 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Net;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using HomeHub.Common.Devices;
-using HomeHub.Common.Exceptions;
-using HomeHub.Common.Trace;
-using HomeHub.Data.Common;
-using HomeHub.Data.Common.Models.Security;
-using HomeHub.Data.Common.Security;
-
-namespace HomeHub.Data.Sql
+﻿namespace HomeHub.Data.Sql
 {
-    public class SqlDataLayer : IDataLayer, ISecurityLayer
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Net;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using HomeHub.Common.Devices;
+    using HomeHub.Common.Exceptions;
+    using HomeHub.Common.Trace;
+    using HomeHub.Data.Common;
+    using HomeHub.Data.Common.Models;
+    using HomeHub.Data.Common.Models.Security;
+    using HomeHub.Data.Common.Security;
+
+    public class SqlDataLayer : IDataLayer, ISecurityLayer, IDisposable
     {
         private readonly SqlConnectionManager connectionManager;
+
+        private readonly CancellationTokenSource tokenSource;
 
         public SqlDataLayer(string connectionString)
         {
             this.connectionManager = new SqlConnectionManager(connectionString);
+            this.tokenSource = new CancellationTokenSource();
+        }
+
+        /// <summary>
+        ///     Dispose everything
+        /// </summary>
+        public void Dispose()
+        {
+            this.tokenSource.Dispose();
+        }
+
+        async Task<Home> IDataLayer.CreateHome(Home home, Guid user)
+        {
+            return await this.connectionManager.ExecuteSql(
+                "hub.createhome", 
+                collection =>
+                    {
+                        collection.AddWithValue("name", home.Name);
+                        collection.AddWithValue("user", user);
+                    },
+                reader =>
+                    {
+                        reader.Read();
+                        return SqlDataLayer.ReadHome(reader);
+                    }, 
+                this.tokenSource.Token);
+        }
+
+        async Task<IEnumerable<Home>> IDataLayer.GetHomes(Guid user)
+        {
+            return await this.connectionManager.ExecuteSql(
+                "hub.gethomes",
+                collection =>
+                    {
+                        collection.AddWithValue("user", user);
+                    },
+                reader =>
+                    {
+                        var result = new List<Home>();
+                        while (reader.Read())
+                        {
+                            result.Add(SqlDataLayer.ReadHome(reader));
+                        }
+                        return result;
+                    },
+                this.tokenSource.Token);
         }
 
         Task<IEnumerable<Device>> IDataLayer.GetAllDevices(Guid user)
         {
-            var lst = new[]
+            var lst = new[] { new Light(Guid.Empty, "device 1"), new Light(Guid.Empty, "device 2") };
+            return Task.FromResult((IEnumerable<Device>)lst);
+        }
+
+        /// <summary>
+        ///     Create a user and get a token for it
+        /// </summary>
+        /// <param name="user">The user info</param>
+        /// <returns>The token</returns>
+        async Task<AuthenticationToken> ISecurityLayer.CreateUser(CreateUser user, IPAddress ip)
+        {
+            var salt = PasswordHelper.GenerateRandom(64);
+            var passBytes = Encoding.Unicode.GetBytes(user.LoginInfo.Password);
+
+            var hashedPass = PasswordHelper.HashPassword(salt, passBytes);
+
+            var token = PasswordHelper.GenerateRandom(64);
+
+            const UserRoles Roles = UserRoles.ADMIN | UserRoles.BASIC;
+
+            return await this.connectionManager.ExecuteSql(
+                "auth.createUser", 
+                collection =>
+                    {
+                        collection.AddWithValue("firstname", user.FirstName);
+                        collection.AddWithValue("lastname", user.LastName);
+                        collection.AddWithValue("email", user.LoginInfo.Email);
+                        collection.AddWithValue("salt", salt);
+                        collection.AddWithValue("password", hashedPass);
+                        collection.AddWithValue("token", token);
+                        collection.AddWithValue("roles", GenerateRolesList(Roles));
+                        collection.AddWithValue("ip", PasswordHelper.GetIPAddressInSqlForm(ip));
+                    }, 
+                reader =>
+                    {
+                        ExceptionUtility.ThrowArgumentExceptionIfFalse(reader.Read(), "Could not create user");
+                        return new AuthenticationToken
+                                        {
+                                            Claims = Roles, 
+                                            Expiration = (DateTime)reader["expiration"], 
+                                            Token = Convert.ToBase64String(token)
+                                        };
+                    }, 
+                this.tokenSource.Token);
+        }
+
+        async Task<User> ISecurityLayer.GetUser(string token, IPAddress ip)
+        {
+            var bytes = Convert.FromBase64String(token);
+
+            var result = await this.connectionManager.ExecuteSql(
+                "auth.getUser", 
+                parameters =>
+                    {
+                        parameters.AddWithValue("token", bytes);
+                        parameters.AddWithValue("ip", PasswordHelper.GetIPAddressInSqlForm(ip));
+                    }, 
+                reader =>
+                    {
+                        if (false == reader.Read())
+                        {
+                            return null;
+                        }
+                        else
+                        {
+                            var user = new User
+                                       {
+                                           Id = (Guid)reader["id"], 
+                                           FirstName = (string)reader["first"], 
+                                           LastName = (string)reader["last"], 
+                                           Created = (DateTime)reader["created"]
+                                       };
+
+                            reader.NextResult();
+
+                            var roles = default(UserRoles);
+                            while (reader.Read())
+                            {
+                                roles |= (UserRoles)Enum.Parse(typeof(UserRoles), (string)reader["claim"]);
+                            }
+
+                            user.Roles = roles;
+
+                            return user;
+                        }
+                    }, 
+                this.tokenSource.Token);
+
+            if (null == result)
             {
-                new Light(Guid.Empty, "device 1"),
-                new Light(Guid.Empty, "device 2")
-            };
-            return Task.FromResult((IEnumerable<Device>) lst);
+                throw new UnauthorizedAccessException("Could not find user.");
+            }
+
+            return result;
         }
 
         async Task<AuthenticationToken> ISecurityLayer.LoginUser(UserPass userpass, IPAddress ip)
@@ -38,22 +176,23 @@ namespace HomeHub.Data.Sql
             var id = default(Guid);
             byte[] salt = null;
             byte[] password = null;
-            await this.connectionManager.ExecuteSql(
-                "auth.getPassword",
-                collection => { collection.AddWithValue("email", userpass.Email); },
-                reader =>
-                {
-                    if (false == reader.Read())
-                    {
-                        HomeHubEventSource.Log.Info($"Could not find user with email: {userpass.Email}");
-                        return;
-                    }
+            await
+                this.connectionManager.ExecuteSql(
+                    "auth.getPassword", 
+                    collection => { collection.AddWithValue("email", userpass.Email); }, 
+                    reader =>
+                        {
+                            if (false == reader.Read())
+                            {
+                                HomeHubEventSource.Log.Info($"Could not find user with email: {userpass.Email}");
+                                return;
+                            }
 
-                    id = (Guid) reader["id"];
-                    salt = (byte[]) reader["salt"];
-                    password = (byte[]) reader["password"];
-                },
-                CancellationToken.None);
+                            id = (Guid)reader["id"];
+                            salt = (byte[])reader["salt"];
+                            password = (byte[])reader["password"];
+                        }, 
+                    CancellationToken.None);
 
             if (null == password)
             {
@@ -72,140 +211,42 @@ namespace HomeHub.Data.Sql
             var claims = default(UserRoles);
 
             await this.connectionManager.ExecuteSql(
-                "auth.loginUser",
+                "auth.loginUser", 
                 collection =>
-                {
-                    collection.AddWithValue("email", userpass.Email);
-                    collection.AddWithValue("password", password);
-                    collection.AddWithValue("token", newToken);
-                    collection.AddWithValue("ip", PasswordHelper.GetIPAddressInSqlForm(ip));
-                },
+                    {
+                        collection.AddWithValue("email", userpass.Email);
+                        collection.AddWithValue("password", password);
+                        collection.AddWithValue("token", newToken);
+                        collection.AddWithValue("ip", PasswordHelper.GetIPAddressInSqlForm(ip));
+                    }, 
                 reader =>
-                {
-                    if (false == reader.Read())
                     {
-                        ExceptionUtility.ThrowFailureException("Could not insert token into db.");
-                    }
-                    id = (Guid) reader["id"];
-                    newToken = (byte[]) reader["token"];
-                    assigned = (DateTime) reader["assigned"];
-                    expiration = (DateTime) reader["expiration"];
-
-                    reader.NextResult();
-
-                    while (reader.Read())
-                    {
-                        var role = (UserRoles) Enum.Parse(typeof(UserRoles), (string) reader["claim"]);
-                        claims |= role;
-                    }
-                },
-                CancellationToken.None);
-
-            return new AuthenticationToken
-            {
-                Token = Convert.ToBase64String(newToken),
-                Claims = claims,
-                Expiration = expiration
-            };
-        }
-
-        /// <summary>
-        ///     Create a user and get a token for it
-        /// </summary>
-        /// <param name="user">The user info</param>
-        /// <returns>The token</returns>
-        async Task<AuthenticationToken> ISecurityLayer.CreateUser(CreateUser user, IPAddress ip)
-        {
-            var salt = PasswordHelper.GenerateRandom(64);
-            var passBytes = Encoding.Unicode.GetBytes(user.LoginInfo.Password);
-
-            var hashedPass = PasswordHelper.HashPassword(salt, passBytes);
-
-            var token = PasswordHelper.GenerateRandom(64);
-
-            var roles = UserRoles.ADMIN | UserRoles.BASIC;
-
-            AuthenticationToken authToken = null;
-
-            await this.connectionManager.ExecuteSql(
-                "auth.createUser",
-                collection =>
-                {
-                    collection.AddWithValue("firstname", user.FirstName);
-                    collection.AddWithValue("lastname", user.LastName);
-                    collection.AddWithValue("email", user.LoginInfo.Email);
-                    collection.AddWithValue("salt", salt);
-                    collection.AddWithValue("password", hashedPass);
-                    collection.AddWithValue("token", token);
-                    collection.AddWithValue("roles", GenerateRolesList(roles));
-                    collection.AddWithValue("ip", PasswordHelper.GetIPAddressInSqlForm(ip));
-                },
-                reader =>
-                {
-                    ExceptionUtility.ThrowArgumentExceptionIfFalse(reader.Read(), "Could not create user");
-                    authToken = new AuthenticationToken
-                    {
-                        Claims = roles,
-                        Expiration = (DateTime) reader["expiration"],
-                        Token = Convert.ToBase64String(token)
-                    };
-                },
-                CancellationToken.None);
-
-            return authToken;
-        }
-
-
-        async Task<User> ISecurityLayer.GetUser(string token, IPAddress ip)
-        {
-            var bytes = Convert.FromBase64String(token);
-
-            var found = true;
-            User user = null;
-
-
-            await this.connectionManager.ExecuteSql(
-                "auth.getUser",
-                parameters =>
-                {
-                    parameters.AddWithValue("token", bytes);
-                    parameters.AddWithValue("ip", PasswordHelper.GetIPAddressInSqlForm(ip));
-                },
-                reader =>
-                {
-                    if (false == reader.Read())
-                    {
-                        found = false;
-                    }
-                    else
-                    {
-                        user = new User
+                        if (false == reader.Read())
                         {
-                            Id = (Guid) reader["id"],
-                            FirstName = (string) reader["first"],
-                            LastName = (string) reader["last"],
-                            Created = (DateTime) reader["created"]
-                        };
+                            ExceptionUtility.ThrowFailureException("Could not insert token into db.");
+                        }
+
+                        id = (Guid)reader["id"];
+                        newToken = (byte[])reader["token"];
+                        assigned = (DateTime)reader["assigned"];
+                        expiration = (DateTime)reader["expiration"];
 
                         reader.NextResult();
 
-                        var roles = default(UserRoles);
                         while (reader.Read())
                         {
-                            roles |= (UserRoles) Enum.Parse(typeof(UserRoles), (string) reader["claim"]);
+                            var role = (UserRoles)Enum.Parse(typeof(UserRoles), (string)reader["claim"]);
+                            claims |= role;
                         }
+                    }, 
+                this.tokenSource.Token);
 
-                        user.Roles = roles;
-                    }
-                },
-                CancellationToken.None);
-
-            if (false == found)
-            {
-                throw new UnauthorizedAccessException("Could not find user.");
-            }
-
-            return user;
+            return new AuthenticationToken
+                       {
+                           Token = Convert.ToBase64String(newToken), 
+                           Claims = claims, 
+                           Expiration = expiration
+                       };
         }
 
         /// <summary>
@@ -220,13 +261,18 @@ namespace HomeHub.Data.Sql
 
             foreach (var role in Enum.GetValues(typeof(UserRoles)))
             {
-                if (roles.HasFlag((UserRoles) role))
+                if (roles.HasFlag((UserRoles)role))
                 {
-                    datatable.Rows.Add(((UserRoles) role).ToString());
+                    datatable.Rows.Add(((UserRoles)role).ToString());
                 }
             }
 
             return datatable;
+        }
+
+        private static Home ReadHome(IDataRecord reader)
+        {
+            return new Home((string)reader["name"], (DateTime)reader["created"], (Guid)reader["id"]);
         }
     }
 }
